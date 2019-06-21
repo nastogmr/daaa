263eefb82eeac6aba944e4dccb2bd61d5cbfecde
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-06-21 19:04:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("2help",{type: 'WATCHING'})
+   client.user.setActivity("1help",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -37,7 +37,7 @@ const queue = new Map();
  
  
  
-var prefix = "2" 
+var prefix = "1" 
 client.on('message', async msg => {
     if (msg.author.bot) return undefined;
    
@@ -230,7 +230,7 @@ function play(guild, song) {
 }
  
 const adminprefix = "$";
-const devs = ['274923685985386496'];
+const devs = ['448469704298856451'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
